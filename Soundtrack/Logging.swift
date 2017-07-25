d984d3b0e7f80542b5dacb9ae590e72a9966a5f5
Manav Rathi <mnvrth@gmail.com>
2017-07-25 12:20:21 +0530

    Remove preemptive thunking
    "The loudspeaker is the most flexible sonic instrument"
    
            – James McCartney (the SuperCollider one)

--- Soundtrack/Logging.swift
+++ Soundtrack/Logging.swift
@@ -8,7 +8,7 @@ import Foundation
 
 /// Writes a log entry.
 
-public func logInfo(_ item: @autoclosure () -> Any, tag: String? = nil, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
+public func logInfo(_ item: Any, tag: String? = nil, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
 
     struct MyDateFormatter {
         static let shared = { () -> DateFormatter in
@@ -24,7 +24,7 @@ public func logInfo(_ item: @autoclosure () -> Any, tag: String? = nil, file: St
     let formattedDate = MyDateFormatter.shared.string(from: date)
     let fileName = (String(describing: file) as NSString).lastPathComponent
 
-    let message = "\(formattedDate) \(fileName):\(line) \(function) \(item())"
+    let message = "\(formattedDate) \(fileName):\(line) \(function) \(item)"
     let taggedMessage: String
     if let tag = tag {
         taggedMessage = "\(tag) " + message
@@ -40,14 +40,14 @@ public func logInfo(_ item: @autoclosure () -> Any, tag: String? = nil, file: St
 
 public func logDebug(_ item: @autoclosure () -> Any, file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
     #if DEBUG
-        logInfo(item, tag: "DEBUG", file: file, line: line, function: function)
+        logInfo(item(), tag: "DEBUG", file: file, line: line, function: function)
     #endif
 }
 
 /// Writes a log entry (always) and terminates execution of debug builds.
 
-public func logWarning(_ item: @autoclosure () -> Any = String(), file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
-    let message = "\(item())"
+public func logWarning(_ item: Any = String(), file: StaticString = #file, line: UInt = #line, function: StaticString = #function) {
+    let message = "\(item)"
     logInfo(message, tag: "WARNING", file: file, line: line, function: function)
     assertionFailure("\(function) \(message)", file: file, line: line)
 }
