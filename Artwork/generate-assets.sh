be1d8dbf8b62b9f3a919b8852223f142d248787d
Manav Rathi <mnvrth@gmail.com>
2017-09-20 14:57:00 +0530

    Generate template icon with the Benzene sign
    From macOS HIGs [1]:
    
        "Consider creating icons as template images. A template image is a
        monochromatic image with transparency, anti-aliasing, and no drop
        shadow that uses a mask to define its shape."
    
        "Consider outputting custom icons in PDF format.  The system
        automatically scales PDF-based icons for high-resolution displays,
        so you don't need to provide high-resolution versions."
    
        "If your icons are in PDF format, only the @1x size is needed."
    
    [1]: https://developer.apple.com/macos/human-interface-guidelines/icons-and-images/custom-icons/

--- Artwork/generate-assets.sh
+++ Artwork/generate-assets.sh
@@ -1,24 +1,17 @@
 #! /bin/sh
 
 INKSCAPE=${INKSCAPE:-/Applications/Inkscape.app/Contents/Resources/bin/inkscape}
-SOURCE_SVG=${SOURCE_SVG:-Artwork/benzene.svg}
+SOURCE_SVG=${SOURCE_SVG:-Artwork/Benzene.svg}
 
 MACOS_APPICONSET=Soundtrack-macOS/Assets.xcassets/AppIcon.appiconset
+MACOS_BENZENE_TEMPLATE_PDF=Soundtrack-macOS/Assets.xcassets/Benzene.imageset
 
 cd $(dirname $0)/..
 src_root=$(pwd)
 
 input_file=${src_root}/${SOURCE_SVG}
 
-output_base_name=benzene
-
-function gen_pdf () {
-    local dest_dir=${src_root}/$1
-
-    ${INKSCAPE} \
-        --file=${input_file} \
-        --export-pdf=${dest_dir}/${output_base_name}.pdf
-}
+output_base_name=Benzene
 
 function gen_png () {
     local dest_dir=${src_root}/$1
@@ -31,11 +24,25 @@ function gen_png () {
         --export-height=${sz}
 }
 
+
+function gen_template_pdf () {
+    local dest_dir=${src_root}/$1
+    
+    local output_file=${dest_dir}/${output_base_name}-Template.pdf
+    ${INKSCAPE} \
+        --file=${input_file} \
+        --export-pdf=${output_file}
+
+    echo "Generated ${output_file}"
+}
+
 function gen_macos_icon () {
     gen_png ${MACOS_APPICONSET} "$@"
 }
 
-# gen_pdf
+function gen_macos_benzene_template_pdf () {
+    gen_template_pdf ${MACOS_BENZENE_TEMPLATE_PDF} "$@"
+}
 
 gen_macos_icon 1024
 gen_macos_icon 512
@@ -44,3 +51,5 @@ gen_macos_icon 128
 gen_macos_icon 64
 gen_macos_icon 32
 gen_macos_icon 16
+
+gen_macos_benzene_template_pdf
