29d614e4873da35bb716a4d2029fd06a85538d06
Manav Rathi <mnvrth@gmail.com>
2017-10-09 12:23:56 +0530

    Teach macOS window about "zoom"

--- Soundtrack-macOS/WindowController.swift
+++ Soundtrack-macOS/WindowController.swift
@@ -6,13 +6,24 @@
 
 import Cocoa
 
-class WindowController: NSWindowController {
+class WindowController: NSWindowController, NSWindowDelegate {
 
     override func windowDidLoad() {
         super.windowDidLoad()
 
         window!.titlebarAppearsTransparent = true
         window!.isMovableByWindowBackground = true
+    }
+
+    func windowWillUseStandardFrame(_ window: NSWindow, defaultFrame newFrame: NSRect) -> NSRect {
+        let defaultSize = NSSize(width: 480, height: 270)
+
+        let currentOrigin = window.frame.origin
+        let currentHeight = window.frame.size.height
+        let currentTopLeft = currentOrigin.y + currentHeight
+        let newOrigin = NSPoint(x: currentOrigin.x, y: currentTopLeft - defaultSize.height)
+
+        return NSRect(origin: newOrigin, size: defaultSize)
     }
 
 }
