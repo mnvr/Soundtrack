faceea826d4c571589a6377cb71555278bb78c35
Manav Rathi <mnvrth@gmail.com>
2017-10-12 15:46:55 +0530

    Animate transitions between play/pause states

--- Soundtrack-macOS/ViewController.swift
+++ Soundtrack-macOS/ViewController.swift
@@ -16,8 +16,13 @@ class ViewController: NSViewController, AudioControllerDelegate, StreamPlayerDel
 
     private var audioController: AudioController!
 
+    private var isPlaying: Bool = false
+    private var currentPlaybackAttempt: Int = 0
+
     private var togglePlaybackMenuItemTitle: String?
     private var togglePlaybackMenuItemIsEnabled: Bool?
+
+    // MARK: -
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,83 +43,132 @@ class ViewController: NSViewController, AudioControllerDelegate, StreamPlayerDel
         return AudioController(url: url, delegate: self, delegateQueue: DispatchQueue.main, makeSession: makeSession)
     }
 
-    // MARK: Actions
+    // MARK: -
 
     @IBAction func play(_ sender: NSButton) {
         log.info("User pressed play")
-        toggle()
+        initiatePlayback()
+        audioController.playPause()
     }
 
     @IBAction func click(_ sender: NSGestureRecognizer) {
         log.info("User clicked inside the window")
-        toggle()
+        initiatePause()
+        audioController.playPause()
     }
 
     @IBAction func togglePlayback(_ sender: NSMenuItem) {
         log.info("User invoked menu toggle")
-        toggle()
-    }
-
-    // MARK: Action Implementations
-
-    func toggle() {
-        indicateUnavailability()
+        isPlaying ? initiatePause() : initiatePlayback()
         audioController.playPause()
     }
 
-    // MARK: Menu Items
-
-    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
-        if menuItem.action == #selector(togglePlayback(_:)) {
-            if let title = togglePlaybackMenuItemTitle {
-                menuItem.title = title
-            }
-            return togglePlaybackMenuItemIsEnabled ?? false
-        }
-
-        return super.validateMenuItem(menuItem)
-    }
-
-    // MARK: State Changes
-
-    private func indicatePlaybackReadiness() {
-        playButton.isEnabled = true
-        playButton.isHidden = false
-        progressIndicator.stopAnimation(self)
-        titleTextField.isHidden = true
-        resetTitle()
-        clickGestureRecognizer.isEnabled = false
-        togglePlaybackMenuItemTitle = NSLocalizedString("Play", comment: "Menu Item - Music > Play")
-        togglePlaybackMenuItemIsEnabled = true
-    }
+    // MARK: -
 
     private func indicateUnavailability() {
+        isPlaying = false
+
         playButton.isEnabled = false
-        playButton.isHidden = true
-        progressIndicator.startAnimation(self)
+        playButton.isHidden = false
+
+        progressIndicator.stopAnimation(self)
+
         titleTextField.isHidden = true
-        resetTitle()
+
         clickGestureRecognizer.isEnabled = false
+
+        togglePlaybackMenuItemTitle = menuTitlePlay()
         togglePlaybackMenuItemIsEnabled = false
     }
 
-    private func indicatePlayback() {
+    private func indicatePlaybackReadiness() {
+        playButton.isEnabled = true
+        togglePlaybackMenuItemIsEnabled = true
+    }
+
+    private func initiatePlayback() {
         playButton.isEnabled = false
-        playButton.isHidden = true
+
+        let playbackAttempt = currentPlaybackAttempt
+        currentPlaybackAttempt += 1
+
+        fadeOut(playButton) { [weak self] in
+            self?.maybeShowProgressIndicatorForPlaybackAttempt(playbackAttempt)
+        }
+
+        togglePlaybackMenuItemIsEnabled = false
+    }
+
+    private func maybeShowProgressIndicatorForPlaybackAttempt(_ playbackAttempt: Int) {
+        if !isPlaying {
+            if playbackAttempt == currentPlaybackAttempt {
+                progressIndicator.startAnimation(self)
+            }
+        }
+    }
+
+    private func indicatePlayback() {
+        isPlaying = true
+
         progressIndicator.stopAnimation(self)
-        resetTitle()
-        titleTextField.isHidden = false
+
         clickGestureRecognizer.isEnabled = true
-        togglePlaybackMenuItemTitle = NSLocalizedString("Pause", comment: "Menu Item - Music > Pause")
+
+        togglePlaybackMenuItemTitle = menuTitlePause()
+        togglePlaybackMenuItemIsEnabled = true
+    }
+
+    private func initiatePause() {
+        isPlaying = false
+
+        fadeOut(titleTextField)
+
+        clickGestureRecognizer.isEnabled = false
+
+        togglePlaybackMenuItemIsEnabled = false
+    }
+
+    private func indicatePause() {
+        playButton.isEnabled = true
+        fadeIn(playButton)
+
+        togglePlaybackMenuItemTitle = menuTitlePlay()
         togglePlaybackMenuItemIsEnabled = true
     }
 
     private func setTitle(_ title: String) {
-        titleTextField.stringValue = title
+        let maybeFadeInTitle = { [weak self] in
+            if !title.isEmpty {
+                if let strongSelf = self, let titleTextField = strongSelf.titleTextField {
+                    titleTextField.stringValue = title
+                    strongSelf.fadeIn(titleTextField)
+                }
+            }
+        }
+
+        if titleTextField.isHidden {
+            maybeFadeInTitle()
+        } else {
+            fadeOut(titleTextField) {
+                maybeFadeInTitle()
+            }
+        }
     }
 
-    private func resetTitle() {
-        setTitle("")
+    // MARK: -
+
+    private func fadeOut(_ view: NSView, duration: TimeInterval = 2, then: (() -> Void)? = nil) {
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = duration
+            view.animator().isHidden = true
+        }, completionHandler: then)
+    }
+
+    private func fadeIn(_ view: NSView, duration: TimeInterval = 2, then: (() -> Void)? = nil) {
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = duration
+            view.animator().isHidden = false
+        }, completionHandler: then)
     }
 
     // MARK: Audio Controller
@@ -132,11 +186,35 @@ class ViewController: NSViewController, AudioControllerDelegate, StreamPlayerDel
     }
 
     func streamPlayerDidStopPlayback(_ streamPlayer: StreamPlayer) {
-        indicatePlaybackReadiness()
+        if isPlaying {
+            initiatePause()
+        }
+        indicatePause()
     }
 
     func streamPlayer(_ streamPlayer: StreamPlayer, didChangeSong title: String) {
         setTitle(title)
+    }
+
+    // MARK: -
+
+    private func menuTitlePlay() -> String {
+        return NSLocalizedString("Play", comment: "Menu Item - Music > Play")
+    }
+
+    private func menuTitlePause() -> String {
+        return NSLocalizedString("Pause", comment: "Menu Item - Music > Pause")
+    }
+
+    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        if menuItem.action == #selector(togglePlayback(_:)) {
+            if let title = togglePlaybackMenuItemTitle {
+                menuItem.title = title
+            }
+            return togglePlaybackMenuItemIsEnabled ?? false
+        }
+
+        return super.validateMenuItem(menuItem)
     }
 
 }
