5e97e12454c34491f79aecb703b8c2a04caa58ed
Manav Rathi <mnvrth@gmail.com>
2017-10-13 18:43:52 +0530

    Touch up the icons

--- Artwork/generate-assets.sh
+++ Artwork/generate-assets.sh
@@ -2,13 +2,14 @@
 
 INKSCAPE=${INKSCAPE:-/Applications/Inkscape.app/Contents/Resources/bin/inkscape}
 
-TRANSPARENT_SVG=${SOURCE_SVG:-Artwork/Benzene.svg}
-FILLED_SVG=${SOURCE_SVG:-Artwork/Benzene-Filled.svg}
+BENZENE_SVG=${BENZENE_SVG:-Artwork/Benzene.svg}
+BENZENE_IOS_SVG=${BENZENE_IOS_SVG:-Artwork/Benzene-iOS.svg}
 
 MACOS_APPICONSET=Soundtrack-macOS/Assets.xcassets/AppIcon.appiconset
 IOS_APPICONSET=Soundtrack-iOS/Assets.xcassets/AppIcon.appiconset
 
 MACOS_BENZENE_TEMPLATE_PDF=Soundtrack-macOS/Assets.xcassets/Benzene.imageset
+IOS_BENZENE_TEMPLATE_PDF=Soundtrack-iOS/Assets.xcassets/Benzene.imageset
 
 cd $(dirname $0)/..
 src_root=$(pwd)
@@ -19,11 +20,12 @@ function gen_png () {
     local input_file=${src_root}/$1
     local dest_dir=${src_root}/$2
     local sz=${3:-1024}
+    local suffix=$4
 
     ${INKSCAPE} \
         --without-gui \
         --file=${input_file} \
-        --export-png=${dest_dir}/${output_base_name}-${sz}.png \
+        --export-png=${dest_dir}/${output_base_name}-${sz}${suffix}.png \
         --export-width=${sz} \
         --export-height=${sz}
 }
@@ -42,16 +44,23 @@ function gen_template_pdf () {
 }
 
 function gen_macos_icon () {
-    gen_png "${TRANSPARENT_SVG}" "${MACOS_APPICONSET}" "$@"
+    gen_png "${BENZENE_SVG}" "${MACOS_APPICONSET}" "$@"
 }
 
-function gen_macos_benzene_template_pdf () {
-    gen_template_pdf "${TRANSPARENT_SVG}" "${MACOS_BENZENE_TEMPLATE_PDF}" "$@"
+function gen_benzene_template_pdf () {
+    gen_template_pdf "${BENZENE_SVG}" "${MACOS_BENZENE_TEMPLATE_PDF}" "$@"
+    gen_template_pdf "${BENZENE_SVG}" "${IOS_BENZENE_TEMPLATE_PDF}" "$@"
 }
 
 function gen_ios_icon () {
-    gen_png "${FILLED_SVG}" "${IOS_APPICONSET}" "$@"
+    gen_png "${BENZENE_IOS_SVG}" "${IOS_APPICONSET}" "$@"
 }
+
+function gen_ios_appstore_icon () {
+    gen_png "${BENZENE_IOS_SVG}" "Artwork" 1024 "-iOS"
+}
+
+gen_benzene_template_pdf
 
 gen_macos_icon 1024
 gen_macos_icon 512
@@ -61,9 +70,6 @@ gen_macos_icon 64
 gen_macos_icon 32
 gen_macos_icon 16
 
-gen_macos_benzene_template_pdf
-
-gen_ios_icon 1024
 gen_ios_icon 180
 gen_ios_icon 120
 gen_ios_icon 167
@@ -77,3 +83,5 @@ gen_ios_icon 58
 gen_ios_icon 40
 gen_ios_icon 29
 gen_ios_icon 20
+
+gen_ios_appstore_icon
